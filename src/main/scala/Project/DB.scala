1876cb44733e87b3245ef69c56c9dcb05ad27374
Farah <farah.elyamany1234@gmail.com>
2026-04-17 00:10:56 +0200

    feat(Loading): Created DB Helper for Connection & Insertion Data

---
+++ src/main/scala/Project/DB.scala
@@ -0,0 +1,52 @@
+package Project
+import java.sql.{Connection, DriverManager}
+import scala.util.{Try, Using}
+import java.time.Instant
+import java.sql.Timestamp
+
+object DB {
+  val url : String= "jdbc:oracle:thin:@localhost:1521:XE"
+  val user : String= "scala_project"
+  val password : String= "123"
+
+  //[A] means it can work with any type and return it
+  def withConnection[A](f: Connection => A): Try[A] = {
+    Using(DriverManager.getConnection(url, user, password)) { conn =>
+      f(conn)
+    }
+  }
+
+  def insertOrder(
+                   conn: Connection,
+                   order: (String, String, String, Int, Double, String, String),
+                   originalPrice: Double,
+                   discount: Double,
+                   finalPrice: Double
+                 ): Int = {
+
+    val sql =
+      """INSERT INTO orders_processed
+        |(transaction_date, product_name, expiry_date, quantity, unit_price,
+        | channel, payment_method, original_price, discount, final_price)
+        |VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """.stripMargin
+
+    val stmt = conn.prepareStatement(sql)
+
+    stmt.setTimestamp(1, Timestamp.from(Instant.parse(order._1)))
+    stmt.setString(2, order._2)
+    stmt.setDate(3, java.sql.Date.valueOf(order._3))
+    stmt.setInt(4, order._4)
+    stmt.setDouble(5, order._5)
+    stmt.setString(6, order._6)
+    stmt.setString(7, order._7)
+    stmt.setDouble(8, originalPrice)
+    stmt.setDouble(9, discount)
+    stmt.setDouble(10, finalPrice)
+
+    val rows = stmt.executeUpdate()
+    stmt.close()
+    rows
+  }
+
+}
